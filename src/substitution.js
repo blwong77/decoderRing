1e886283edeb89dea17958be0be287dab661624d
Bradley Wong <blwong77@gmail.com>
2021-11-22 09:00:17 -0800

    added some comments

--- src/substitution.js
+++ src/substitution.js
@@ -3,16 +3,23 @@
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
+// input can include special characters
+// spaces should be maintained
+// capitals can be ignored
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
     const cipher = buildCipher(alphabet);
+    console.log(Object.keys(cipher).length);
+    if(!cipher || Object.keys(cipher).length !== 26) return false;
+    for(let char of input){}
   }
 
   // builds the substitution cipher when given an alphabet
   // returns an object with keys of cipher alphabet and values of std alphabet
   function buildCipher(alphabet){
+    if(!alphabet) return false;
     let asciiKey = 97;
     const cipher = {};
 
@@ -20,10 +27,10 @@ const substitutionModule = (function () {
       cipher[char] = String.fromCharCode(asciiKey);
       asciiKey++;
     }
-    console.log(cipher)
     return cipher;
   }
 
+console.log(substitution("hello", "zxcvbnmasdfghjkl!@#tyuiopq"))
   return {
     substitution,
   };  
